9aca48becbbe1f49c95ba7a86badb9e907f81620
tor4z <vwenjie@hotmail.com>
2018-04-30 21:04:15 +0800

    Add async flow test

---
+++ async_flow.py
@@ -0,0 +1,24 @@
+import asyncio
+import time
+
+async def worker(n):
+    """
+    Some work take time
+    """
+    print("Worker start.")
+    await asyncio.sleep(n)
+    print("Worker done.")
+
+async def main(n):
+    await worker(n)
+    print("Other work")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    start = time.time()
+    loop.run_until_complete(
+        asyncio.gather(
+            main(1), main(2), main(3), main(1)))
+
+    print(f"Took {round(time.time()-start, 3)} Sec.")
