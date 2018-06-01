28cf051d44a8af559554caaf44afacbc5c6ce401
tor4z <vwenjie@hotmail.com>
2018-06-01 13:58:16 +0800

    Update re test

--- re_test.py
+++ re_test.py
@@ -1,10 +1,26 @@
 import re
 
-string = b"how do you turn this on."
+string = "How do you turn this on?"
 
-pat = re.compile(b'do')
+pat = re.compile(r'.*?(?P<do>do).*?[(?P<you>you)|(?P<me>me)]+.*')
 
-s = pat.search(string)
+m = pat.match(string)
 
-start, end = s.span()
-print(string[start:end])
+print(m.group("do"))
+
+print(m.groupdict())
+'''
+print(m.group("you"))
+print(m.group("me"))
+'''
+
+################
+
+string = "How do you 10086 turn this on?"
+
+pat = re.compile(r'.*?(?P<do>do)\s*?\w+\s*(?P<you_d>[\w+|\d+]+).*')
+
+m = pat.match(string)
+
+print(m.groups())
+print(m.groupdict())
