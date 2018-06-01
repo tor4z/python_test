6b18110b4e82ad00a065b03d0ee8f7f331b2f874
tor4z <vwenjie@hotmail.com>
2018-06-01 17:05:16 +0800

    re test

--- re_test.py
+++ re_test.py
@@ -16,11 +16,45 @@ print(m.group("me"))
 
 ################
 
-string = "How do you 10086 turn this on?"
+string = "How do you 1s0086 turn this on?"
 
-pat = re.compile(r'.*?(?P<do>do)\s*?\w+\s*(?P<you_d>[\w+|\d+]+).*')
+pat = re.compile(r'.*?(?P<do>do)\s+\w+\s+(?P<you_d>[a-zA-Z_]\w+|\d+).*')
 
 m = pat.match(string)
 
 print(m.groups())
 print(m.groupdict())
+
+
+######################
+
+string = "ADD R1 R2"
+string2 = "ADD R1 1"
+
+pat = re.compile(r'\s*(?P<ins>[a-zA-Z_]\w*)\s+(?P<arg1>[a-zA-Z_]\w*)\s+(?P<arg2>[a-zA-Z_]\w*|\d+)\s*')
+# pat = re.compile(r'\s*(?P<ins>[a-zA-Z_]\w*)\s+(?P<arg1>[a-zA-Z_]\w*)\s*')
+
+m = pat.match(string)
+print(m.groupdict())
+
+m = pat.match(string2)
+print(m.groupdict())
+
+
+######################
+
+string = " // ADD R1 R2"
+
+pat = re.compile(r'\s*//.*')
+
+m = pat.match(string)
+print(m)
+
+
+######################
+string = "\n"
+
+pat = re.compile(r'\s*\n')
+
+m = pat.match(string)
+print(m)
